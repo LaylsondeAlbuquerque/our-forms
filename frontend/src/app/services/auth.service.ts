6f2452aaddbc2417324040534caed0b5fb9138cb
LaylsondeAlbuquerque <laylsondealbuquerque@gmail.com>
2025-11-19 22:32:30 -0300

    feat: Adiciona configuração do HttpClient e método de cadastro no AuthService

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { CadastroModel } from '../models/cadastro.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:3000/api';
+
+  constructor () {}
+
+  // Método para enviar os dados de cadastro para o back-end
+  cadastrar (usuario: CadastroModel): Observable<any> {
+    return this.http.post(`${this.apiUrl}/cadastro`, usuario);
+  }
 }
