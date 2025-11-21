b6c039a285757557d49e5da0b0b54810d926fa2b
LaylsondeAlbuquerque <laylsondealbuquerque@gmail.com>
2025-11-21 16:12:12 -0300

    feat(frontend): implement login form logic and data binding
    - Creates LoginModel interface
    - Configures component login with FormsModule and local state
    - Binds HTML inputs to component model using ngModel

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { LoginModel } from '../../models/login.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login', 
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export default class Login {
+
+  novoLogin: LoginModel = {
+    email: '',
+    senha: '',
+  }
+
+  onSubmit() {
+    console.log('Tentativa de login: ', this.novoLogin);
+  }
 
 }
