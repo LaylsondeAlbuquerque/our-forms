14855512d89f7630e2f178891559d63ddffee379
LaylsondeAlbuquerque <laylsondealbuquerque@gmail.com>
2025-11-23 14:04:54 -0300

    feat(frontend): implement login component logic and API integration
    - Injects AuthService
    - Implements onSubmit handler with form validation check
    - Connects login form to AuthService.login()
    - Adds success handling (token storage) and error handling (user alerts)
    - Uses markAllAsTouched for visual validation feedback

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LoginModel } from '../../models/login.model';
-import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login', 
@@ -14,14 +15,44 @@ import { RouterLink } from '@angular/router';
   styleUrl: './login.css',
 })
 export default class Login {
-
+  // Dados de login
   novoLogin: LoginModel = {
     email: '',
     senha: '',
   }
 
-  onSubmit() {
-    console.log('Tentativa de login: ', this.novoLogin);
+  // Injects
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  // Método para submeter o formulário
+  onSubmit(form: NgForm) {
+    
+    // Se o formulário tiver algum erro
+    if (form.invalid) {
+      console.log("Formulário inválido, não enviado.");
+      form.control.markAllAsTouched();
+      return;
+    }
+
+    // Executa o Observable: Envia o Post para a API e gerencia os callbacks de resposta
+    this.authService.login(this.novoLogin).subscribe({
+
+      // Se der certo:
+      next: (resposta) => {
+        console.log('Sucesso!', resposta);
+        this.authService.setToken(resposta.token);
+        alert('Bem-vindo de volta!');
+      },
+
+      // Se der errado:
+      error: (erro) => {
+        console.error('Erro no login: ', erro);
+        alert(erro.error?.message || 'Ocorreu um erro ao tentar logar.');
+      }
+    });
+
+
   }
 
 }
