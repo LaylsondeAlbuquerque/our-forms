ba36d8cb78cebed590d9f448a8f2c34d20c00f9f
LaylsondeAlbuquerque <laylsondealbuquerque@gmail.com>
2025-11-23 00:39:51 -0300

    feat(frontend): add login and token management to AuthService
    - Implements login method with HTTP POST to backend API
    - Adds methods to set, get, and remove JWT token from localStorage
    - Updates method signature to use strict LoginModel typing

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { CadastroModel } from '../models/cadastro.model';
 import { Observable } from 'rxjs';
+import { LoginModel } from '../models/login.model';
 
 @Injectable({
   providedIn: 'root',
@@ -16,5 +17,21 @@ export class AuthService {
   // Método para enviar os dados de cadastro para o back-end
   cadastrar (usuario: CadastroModel): Observable<any> {
     return this.http.post(`${this.apiUrl}/cadastro`, usuario);
+  }
+
+  // Método para enviar os dados de login para o back-end
+  login (usuario: LoginModel): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, usuario);
+  }
+
+  // Métodos para manipulação do token
+  setToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+  removeToken(): void {
+    localStorage.removeItem('token');
   }
 }
