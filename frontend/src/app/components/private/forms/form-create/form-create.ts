e0dd7fe3e1795e2a3edd6af16feb7189804d0e07
Laylson de Albuquerque <laylsondealbuquerque@gmail.com>
2025-12-17 10:22:24 -0300

    feat(frontend): implement drag-and-drop functionality for form questions in file .ts

--- frontend/src/app/components/private/forms/form-create/form-create.ts
+++ frontend/src/app/components/private/forms/form-create/form-create.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-form-create',
-  imports: [],
+  imports: [CommonModule, DragDropModule],
   templateUrl: './form-create.html',
   styleUrl: './form-create.css',
 })
 export class FormCreate {
 
+  perguntas = [
+    { titulo: 'Qual seu nome?', tipo: 'Texto Curto' },
+    { titulo: 'Qual sua idade?', tipo: 'Número' },
+    { titulo: 'Gosta de Angular?', tipo: 'Múltipla Escolha' }
+  ];
+
+  soltar(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.perguntas, event.previousIndex, event.currentIndex);
+  }
 }
