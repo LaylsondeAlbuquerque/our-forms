2731b6dd108b940822fd62eba727256de9722b5f
Laylson de Albuquerque <laylsondealbuquerque@gmail.com>
2026-01-21 22:38:54 -0300

    refactor: remove unused RouterLink import from form-create component

--- frontend/src/app/components/private/forms/form-create/form-create.ts
+++ frontend/src/app/components/private/forms/form-create/form-create.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
-import { RouterLink } from "@angular/router";
 import { Question, QuestionType } from '../../../../models/question.model';
 import { UiSelect } from '../../../shared/features/ui-select/ui-select';
 
 @Component({
   selector: 'app-form-create',
-  imports: [CommonModule, DragDropModule, RouterLink, UiSelect],
+  imports: [CommonModule, DragDropModule, UiSelect],
   templateUrl: './form-create.html',
   styleUrl: './form-create.css',
 })
